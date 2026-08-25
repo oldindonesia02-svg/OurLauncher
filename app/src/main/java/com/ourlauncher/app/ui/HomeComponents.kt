ea2d6827c03b9200e2598cf6169ab52bc8691274
oldindonesia02-svg <oldindonesia02@gmail.com>
2026-08-25 09:00:21 +0530

    Update HomeComponents.kt

--- app/src/main/java/com/ourlauncher/app/ui/HomeComponents.kt
+++ app/src/main/java/com/ourlauncher/app/ui/HomeComponents.kt
@@ -34,6 +34,7 @@ import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.graphicsLayer
